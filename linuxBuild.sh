3ed0f89be92d2139a62af428cd97129c3d57b291
OriginatorX <fetison50@gmail.com>
2021-09-16 14:21:40 +1000

    fix bugs

--- linuxBuild.sh
+++ linuxBuild.sh
@@ -1,10 +1,10 @@
 echo "Building for linux"
 
-if [ ! -d "$build/" ]; then
+if [ ! -d "$./build" ] 
+then
+    echo "HI"
     mkdir build && echo DIR build created
 fi
 
 cd build
 make -f ../Makefile
-
-
